33685232dd208259cfbd166995396742818fa87d
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-06-07 17:23:40 +0100

    thermostat logic completed

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -72,4 +72,30 @@ describe('Thermostat', () => {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
+  describe('displaying usage levels', () => {
+    describe('when the temperature is below 18 degrees', () => {
+      it('it is described as low-usage', () => {
+        for (let i = 0; i < 3; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+    
+    describe('when the temperature is between 18 and 25 degrees', () => {
+      it('it is described as medium-usage', () => {
+        expect(thermostat.energyUsage()).toEqual('medium-usage');
+      });
+    });
+    
+    describe('when the temperature is above 25 degrees', () => {
+      it('is considered high-usage', () => {
+        thermostat.powerSavingMode = false;
+        for (let i = 0; i < 6; i++) {
+          thermostat.up();
+        }
+        expect(thermostat.energyUsage()).toEqual('high-usage');
+      });
+    });
+  });
 });
